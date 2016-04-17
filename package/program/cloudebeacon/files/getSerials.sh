a200b36e7dc60d50f31a881fd8a78d5a39e95760
hxw804781317 <huangxiaowen@126.com>
2016-04-17 09:12:06 +0800

    del some file

--- package/program/cloudebeacon/files/getSerials.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-filePath="/usr/ibeacon/conf/ssid.conf"
-
-#grep "^cb" "$filePath" | cut -d'=' -f2
-cat ${filePath}
--- package/program/cloudebeacon/src/netPing.h
+++
@@ -1,8 +0,0 @@
-#ifndef __NET_PING_H__
-#define  __NET_PING_H__
-
-int ping_main( const char *host,int flag);
-int GetPingRecieveCount();
-
-#endif
-
